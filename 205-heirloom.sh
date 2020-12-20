bb22bcb290f95b2f1b569343d1087c1f31dee14f
Dan <dan@localhost.localdomain>
2020-12-21 07:44:31 +1100

    New SVR4 package heirloom.

---
+++ 205-heirloom.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# mount/wall disabled for now since it wants to chggrp
+#
+
+# setup
+
+PKGNAME=heirloom-040306
+NPROC=`nproc`
+TOPLEVEL=`pwd`
+PKGDIR=${TOPLEVEL}/pkgbuild/${PKGNAME}
+cd src
+rm -rf ./${PKGNAME}
+tar -zxvf ${PKGNAME}.tar.gz
+cd ${PKGNAME} 
+
+# configure/build/install
+
+make  
+sudo make ROOT=${PKGDIR} install
+sudo rm -f ${PKGDIR}/var/log/sulog
+
+# package
+
+cd ${PKGDIR}
+
+cat <<__PKGINFO__ > pkginfo
+PKG=S5LXheirloom-tools
+NAME=${PKGNAME}
+DESC=heirloom-tools
+VENDOR=Headrat Linux
+VERSION=000000
+ARCH=IA64,x86_64
+CATEGORY=utilities
+BASEDIR=/
+__PKGINFO__
+
+../../mkproto.sh
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/5bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/5bin
 cd /packages
-for PKG in S5LXglibc S5LXncurses S5LXutillinux S5LXcoreutils S5LXbash S5LXwhich S5LXstrace ; do 
+for PKG in S5LXglibc S5LXncurses S5LXutillinux S5LXcoreutils S5LXbash S5LXwhich S5LXstrace S5LXheirloom-tools ; do 
 	PKGSIZE=`/usr/5bin/ls -sh /packages/${PKG}.pkg  | /usr/5bin/awk '{ print $1; }'`
 	echo "Installing package ${PKG} (${PKGSIZE}) ... "
 	( echo 1 && yes) | pkgadd -d ${PKG}.pkg  1>/dev/null 2>&1
