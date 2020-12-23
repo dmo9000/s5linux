f67f2594af16768f9de59c13e0204764fb541070
dmo9000 <dan@localhost.localdomain>
2020-12-24 08:41:43 +1100

    Modify bootstrap to allow package installation from arbitrary path, eg. from DVD.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/5bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/5bin
-cd /packages
+PKGPATH=`basename ${1}`
+cd ${PKGPATH}
 for PKG in `/usr/5bin/cat ${1}` ; do 
-	PKGSIZE=`/usr/5bin/ls -sh /packages/${PKG}.pkg  | /usr/5bin/awk '{ print $1; }'`
+	PKGSIZE=`/usr/5bin/ls -sh ${PKGPATH}/${PKG}.pkg  | /usr/5bin/awk '{ print $1; }'`
 	echo "Installing package ${PKG} (${PKGSIZE}) ... "
 	( echo 1 && yes) | pkgadd -d ${PKG}.pkg  1>/dev/null 2>&1
 	/sbin/ldconfig
