d48b48e30840157a7da5a202abd4a6f9ad45d431
dmo9000 <dan@localhost.localdomain>
2020-12-23 15:02:25 +1100

    Use system installed pkgproto again now that it's fixed.

--- mkproto.sh
+++ mkproto.sh
@@ -3,7 +3,7 @@
 rm -f prototype file.list
 find . ! -name "pkginfo" ! -name "prototype" ! -name "file.list" > file.list
 md5sum file.list
-#(echo "i pkginfo" && stdbuf -o0 /usr/ccs/bin/pkgproto < file.list) | sed -e "s/dan dan$/root root/g" > prototype && rm -f file.list
-(echo "i pkginfo" && stdbuf -o0 ../../utils/pkgproto < file.list) | sed -e "s/dan dan$/root root/g" > prototype && rm -f file.list
+(echo "i pkginfo" && stdbuf -o0 /usr/ccs/bin/pkgproto < file.list) | sed -e "s/dan dan$/root root/g" > prototype && rm -f file.list
+#(echo "i pkginfo" && stdbuf -o0 ../../utils/pkgproto < file.list) | sed -e "s/dan dan$/root root/g" > prototype && rm -f file.list
 
 md5sum prototype
