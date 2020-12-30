d5afd9e00cdcb15b70afbc1b33f5800af13b2fcf
dmo9000 <dan@localhost.localdomain>
2020-12-30 13:01:57 +1100

    Progress bar for base image installer using pv.

--- install-sda.sh
+++ install-sda.sh
@@ -4,39 +4,34 @@
 #echo "*** Read to install to /dev/sda2 - hit ENTER to start"
 #read ENTER
 
-echo "** Wiping partition table on /dev/sda ..."
+echo "*** Wiping partition table on /dev/sda ..."
 /sbin/wipefs --force -a /dev/sda 1>/dev/null 2>&1
 
-echo "*** Creating /boot and /root partitions ..."
+echo "  *** Creating /boot and /root partitions ..."
 
 #echo ';' | /sbin/sfdisk /dev/sda 1>/dev/null 2>&1 
 printf "type=83,size=100M,bootable\ntype=83\n" | /sbin/sfdisk /dev/sda 1>/dev/null 2>&1
 
-echo "*** Formatting /boot (/dev/sda1) as ext4 ..."
+echo "  *** Formatting /boot (/dev/sda1) as ext4 ..."
 /sbin/wipefs --force -a /dev/sda1 1>/dev/null 2>&1
 /sbin/mkfs.ext4 /dev/sda1 1>/dev/null 2>&1
 
 echo "*** Copying base install image ... "
 /sbin/wipefs --force -a /dev/sda2 1>/dev/null 2>&1
-/bin/dd if=/root/rootfs.ext4 of=/dev/sda2 bs=100M 1>/dev/null 2>&1 1>/dev/null 2>&1
+#/bin/dd if=/root/rootfs.ext4 of=/dev/sda2 bs=100M 1>/dev/null 2>&1 1>/dev/null 2>&1
+/usr/bin/pv /root/rootfs./ext4 > /dev/sda2 
 
-echo "*** Checking filesystem integrity ..."
+echo "  *** Checking filesystem integrity ..."
 
 /sbin/e2fsck -f /dev/sda2 1>/dev/null 2>&1
 
-echo "*** Expanding filesystem on /dev/sda2 ..."
-
-echo "*** Mounting new filesystem ..."
+echo "  *** Expanding filesystem on /dev/sda2 ..."
+echo "    *** Mounting new filesystem ..."
 mount /dev/sda2 /mnt/install 1>/dev/null 2>&1
-
-echo "*** Expanding filesystem on /dev/sda2 ..."
+echo "    *** Resizing new filesystem ..."
 /sbin/resize2fs /dev/sda2 1>/dev/null 2>&1
 
 echo "*** Doing some minor housekeeping ... "
-
-rm -f /mnt/install/etc/fstab
-echo "/dev/sda1 /mnt/install/boot ext4 defaults 0 0" >> /mnt/install/etc/fstab
-echo "/dev/sda2 /mnt/install ext4 defaults 0 0" >> /mnt/install/etc/fstab
 
 rm -rf /mnt/install/boot/bzImage
 rm -rf /mnt/install/rr_moved 
