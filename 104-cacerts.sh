a665f7eba3f9b959fd2bce96b0a4b46c35dd8329
dmo9000 <dan@localhost.localdomain>
2020-12-30 12:05:26 +1100

    Minor cleanups, CA cert package.

---
+++ 104-cacerts.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+
+# setup
+
+PKGNAME=cacerts-1.0
+NPROC=`nproc`
+TOPLEVEL=`pwd`
+PKGDIR=${TOPLEVEL}/pkgbuild/${PKGNAME}
+
+# configure/build/install
+
+mkdir -p ${PKGDIR}/etc/pki/tls/certs/
+cp -rfpv ${TOPLEVEL}/configs/etc/pki/tls/certs/ca-bundle.crt ${PKGDIR}/etc/pki/tls/certs/
+
+cd ${TOPLEVEL}
+
+# package
+
+cd ${PKGDIR}
+
+cat <<__PKGINFO__ > pkginfo
+PKG=S5LXcacerts
+NAME=${PKGNAME}
+DESC=TLS CA Certs
+VENDOR=HeadRat Linux
+VERSION=1.0
+ARCH=IA64,x86_64
+CATEGORY=libraries
+BASEDIR=/
+__PKGINFO__
+
+../../mkproto.sh
+../../mkpkg.sh
