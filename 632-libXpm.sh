eba230300c0c0732980fa7a2164cc53b324f60f8
dmo9000 <dan@localhost.localdomain>
2021-01-02 13:51:01 +1100

    More X stuff.

---
+++ 632-libXpm.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+
+# setup
+#
+set -e
+#
+# libXpm
+PKGID=S5LXlibXpm
+PKG=libXpm
+VERSION=3.5.13
+PKGNAME=${PKG}-${VERSION}
+NPROC=`nproc`
+TOPLEVEL=`pwd`
+PKGDIR=${TOPLEVEL}/pkgbuild/${PKGNAME}
+cd src
+rm -rf ./${PKGNAME}
+tar -zxvf ${PKGNAME}.tar.gz
+cd ${PKGNAME} 
+
+rm -rf ${PKGDIR}
+# configure/build/install
+./configure --prefix=/usr
+make  -j ${NPROC}
+make install DESTDIR=${PKGDIR}
+# package
+
+cd ${PKGDIR}
+
+cat <<__PKGINFO__ > pkginfo
+PKG=${PKGID}
+NAME=${PKGNAME} utilities
+DESC=libXpm
+VENDOR=HeadRat Linux
+VERSION=${VERSION}
+ARCH=x86_64
+CATEGORY=utilities
+BASEDIR=/
+__PKGINFO__
+
+cat <<__POSTINSTALL__ > postinstall
+#!/bin/sh
+/sbin/ldconfig
+__POSTINSTALL__
+
+
+../../mkproto.sh
+../../mkpkg.sh
